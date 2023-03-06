531207bc588e480719ac33cd97cdc4a0a6794355
Duck155 <78767221+Duck155@users.noreply.github.com>
2023-03-06 11:55:20 -0500

    asdasd
    sadads

---
+++ static/apps.js
@@ -0,0 +1,62 @@
+var apps = [
+  {
+    "id": "google",
+    "title": "Google",
+    "url": "https://google.com"
+  },
+  {
+    "id": "discord",
+    "title": "Discord",
+    "url": "https://discord.com/app"
+  },
+  {
+    "id": "geforce",
+    "title": "GeForce NOW",
+    "url": "https://play.geforcenow.com"
+  },
+  {
+    'id': 'vscode',
+    'title': 'Visual Studio Code',
+    'url': 'https://vscode.dev/'
+  },
+  {
+    "id": "tiktok",
+    "title": "TikTok",
+    "url": "https://tiktok.com"
+  },
+  {
+    "id": "reddit",
+    "title": "Reddit",
+    "url": "https://reddit.com"
+  },
+  {
+    "id": "youtube",
+    "title": "YouTube",
+    "url": "https://youtube.com"
+  },
+  {
+    "id": "google",
+    "title": "Google",
+    "url": "https://google.com"
+  },
+  {
+    "id": "twitter",
+    "title": "Twitter",
+    "url": "https://twitter.com"
+  },
+  {
+    'id': 'spotify',
+    'title': 'Spotify',
+    'url': 'https://open.spotify.com/browse'
+  },
+  {
+    'id': 'chess',
+    'title': 'Chess.com',
+    'url': 'https://chess.com'
+  },
+  {
+    'id': 'sflix',
+    'title': 'SFlix',
+    'url': 'https://sflix.to'
+  }
+]
---
+++ static/games.js
@@ -0,0 +1,113 @@
+var games = [
+  {
+    'id': 'idlebreakout',
+    'title': 'Idle Breakout',
+    'url': 'https://sobloxsy.github.io/idle-breakout'
+  },
+  {
+    'id': 'cuttherope',
+    'title': 'Cut the Rope',
+    'url': 'https://yell0wsuit.page/assets/games/CutTheRope/'
+  },
+  {
+    'id': 'papaburgers',
+    'title': 'Papa\'s Burgeria',
+    'url': 'https://mazahacka2017.github.io/papas-burgeria/'
+  },
+  {
+    'id': 'callofduty',
+    'title': 'Call of Duty',
+    'url': 'https://nowgg.me/cod/cod.html',
+    'text': 'May take a while to load'
+  },
+  {
+    'id': 'impossiblequiz',
+    'title': 'The Impossible Quiz',
+    'url': 'https://proudparrot2.github.io/msg-archive/g_mes/impossiblequiz1/impossiblequiz1/index.html'
+  },
+  {
+    'id': 'retrobowl',
+    'url': 'https://proudparrot2.github.io/retro-bowl/retro-bowl/index.html',
+    'title': 'Retro Bowl'
+  },
+  {
+    'id': 'roblox',
+    'title': "Roblox",
+    'url': 'https://nowgg.me/old/roblox.html'
+  },
+  {
+    'id': 'slope',
+    'title': 'Slope',
+    'url': 'https://mathadventure1.github.io/slope/slope/index.html'
+  },
+  {
+    "id": '1v1lol',
+    'title': "1v1.lol",
+    'url': 'https://docs-opensocial.googleusercontent.com/gadgets/ifr?url=https://cdn.jsdelivr.net/gh/ko1ov/to@1e8a949f89fcf2b110640c41a0705db01405d161/d7uf4.xml#'
+  },
+  {
+    'id': 'crossyroad',
+    'title': 'Crossy Road',
+    'url': 'https://yell0wsuit.page/assets/games/crossy-road'
+  },
+  {
+    'id': 'subwaysurfers',
+    'title': 'Subway Surfers',
+    'url': 'https://yell0wsuit.page/assets/games/subway-surfers-unity'
+  },
+  {
+    'id': '8ballpool',
+    'title': '8 Ball Pool',
+    'url': 'https://8ball-pool.io'
+  },
+  {
+    'id': 'fruitninja',
+    'title': 'Fruit Ninja',
+    'url': 'https://yell0wsuit.page/assets/games/fruitninja'
+  },
+  {
+    'id': 'chromedino',
+    'title': "Chrome Dino Game",
+    'url': 'https://yell0wsuit.page/assets/games/chrome-dino'
+  },
+  {
+    'id': 'doodlejump',
+    'title': 'Doodle Jump',
+    'url': 'https://yell0wsuit.page/assets/games/doodle-jump'
+  },
+  {
+    'id': 'slither',
+    'title': 'slither.io',
+    'url': 'https://slither.io'
+  },
+  {
+    'id': 'basketballstars',
+    'title': 'Basketball Stars',
+    'url': 'https://rawcdn.githack.com/3kh0/3kh0-Assets/d1a97cbddcf69b15adf2ab4a491df717ecf22938/basketball-stars/index.html'
+  },
+  {
+    'id': 'amongus',
+    'title': 'Among Us',
+    'url': 'https://rawcdn.githack.com/3kh0/3kh0-Assets/d1a97cbddcf69b15adf2ab4a491df717ecf22938/among-us/index.html'
+  },
+  {
+    'id': 'rocketleague',
+    'title': 'Rocket League',
+    'url': 'https://rawcdn.githack.com/3kh0/3kh0-Assets/d1a97cbddcf69b15adf2ab4a491df717ecf22938/Rocket-League/index.html'
+  },
+  {
+    'id': 'bitlife',
+    'title': 'Bitlife',
+    'url': 'https://rawcdn.githack.com/3kh0/3kh0-Assets/d1a97cbddcf69b15adf2ab4a491df717ecf22938/bitlife/index.html'
+  },
+  {
+    'id': 'btd4',
+    'title': 'Bloons TD 4',
+    'url': 'https://raw.githack.com/3kh0/3kh0-Assets/d1a97cbddcf69b15adf2ab4a491df717ecf22938/btd4/index.html'
+  },
+  {
+    'id': 'ovo',
+    'title': 'OvO',
+    'url': 'https://purepro4561.github.io/OvO/'
+  }
+]
